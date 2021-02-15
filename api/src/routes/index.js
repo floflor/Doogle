5c8b490be1148074071afb04a273c77e31435ad8
Flor <inquires.flo@gmail.com>
2021-02-15 14:34:26 -0300

    merged route dogs and dogs?name

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -12,35 +12,87 @@ router.use(bodyParser.json());
 
 const key = '969b5fbc-8212-491d-80f3-a84877eab206';
 
-// GET /dogs
-router.get('/dogs', (req, res) => {
-    fetch('https://api.thedogapi.com/v1/breeds')
-        .then(r => r.json())
-        .then(data => {
-            var myRes = [];
-            for (let i = 0; i < 8; i++) {
-                var random = Math.floor(Math.random() * 100) + 1;
-                myRes.push(data[random]);
-            }
-            res.json(myRes);
-        })
 
+
+// GET /dogs && dogs?name
+router.get('/dogs', (req, res) => {
+    console.log('entro');
+    //if query attached
+    if (req.query.name) {
+        fetch(`https://api.thedogapi.com/v1/breeds/search?name=${req.query.name}&apikey=${key}`)
+            .then(r => r.json())
+            .then(data => {
+                var myRes = [];
+
+                for (let i = 0; i < 8 && i < data.length; i++) {
+                    myRes.push(data[i])
+                }
+
+                res.json(myRes)
+            });
+    }
+    //if just /dog
+    else {
+        fetch('https://api.thedogapi.com/v1/breeds')
+            .then(r => r.json())
+            .then(data => {
+                var myRes = [];
+                for (let i = 0; i < 8; i++) {
+                    var random = Math.floor(Math.random() * 100) + 1;
+                    var myObj = {
+                        temperament: data[random].temperament,
+                        name: data[random].name,
+                        image: data[random].image
+                    }
+                    myRes.push(myObj);
+                }
+                res.json(myRes);
+            })
+    }
 });
 
 
-// GET /dogs?name=
-router.get('/dogs', (req, res) => {
-    fetch(`https://api.thedogapi.com/v1/breeds/search?q=${req.query.name}&apikey=${key}`)
+
+
+// GET dogs/{idRaza}
+
+router.get('/dogs/:idRaza', (req, res) => {
+    console.log(req.params.idRaza);
+    fetch(`https://api.thedogapi.com/v1/breeds/search?name=${req.params.idRaza}&apikey=${key}`)
         .then(r => r.json())
         .then(data => {
-            var myRes = [];
 
-            for (let i = 0; i < 8 && i < data.length; i++) {
-                myRes.push(data[i])
-            }
+            var referenceImg = data[0].reference_image_id;
 
-            res.json(myRes)
+
+            fetch(`https://api.thedogapi.com/v1/images/${referenceImg}`)
+                .then(r => r.json())
+                .then(dt => {
+                    console.log(referenceImg)
+                    var myArr = [];
+
+                    var myObj = {
+                        temperament: dt.breeds[0].temperament,
+                        name: dt.breeds[0].name,
+                        image: dt.url
+                    }
+
+                    myArr.push(myObj);
+
+
+                    return res.json(myArr);
+                })
+            return;
         })
 });
+
+
+// GET /temperament 
+
+router.get('/temperament', (req, res) => {
+
+})
+
+
 
 module.exports = router;
